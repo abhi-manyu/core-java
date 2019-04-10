79996d4004329fde196926f995ae0655c9d31c54
Abhi <abhim.moharana@gmail.com>
2019-04-10 13:23:56 +0530

    added first userdefined method

--- GIT example/src/org/git/example/Demo1.java
+++ GIT example/src/org/git/example/Demo1.java
@@ -2,7 +2,14 @@ package org.git.example;
 
 public class Demo1
 {
-   public static void main(String[] args) {
+   public static void main(String[] args)
+   {
 	System.out.println("this is main method");
-}
+	first();
+   }
+   
+   public static void first()
+   {
+	   System.out.println("this is the first method");
+   }
 }
