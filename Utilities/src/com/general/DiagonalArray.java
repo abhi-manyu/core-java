9ac6d63c960e3537908f2a1155b0fd977b14202c
Abhi <abhim.moharana@gmail.com>
2019-04-25 10:59:35 +0530

    printing both side diagonal elements as '@' and '*'

--- Utilities/src/com/general/DiagonalArray.java
+++ Utilities/src/com/general/DiagonalArray.java
@@ -6,8 +6,8 @@ public class DiagonalArray
 	//printing the diagonal element difference
 	public static void main(String[] args)
 	{
-		int arr[][]={{1,2,3,5},{2,4,5,7},{4,4,5,8},
-				           {4,6,8,0}};
+		int arr[][]={{1,2,3,5,6,7},{2,4,5,7,6,7},{4,4,5,8,6,7},
+				           {4,6,8,0,6,7},{4,6,8,0,6,7},{4,6,8,0,6,7}};
 		for(int i=0;i<arr.length;i++)
 		{
 		    for(int j=0;j<arr[i].length;j++)
@@ -15,8 +15,10 @@ public class DiagonalArray
 			   /*printing all array element
 			   System.out.print(arr[i][j]+"  ");*/
 				//printing only diagonal elements
-					 if(i+j==(arr[i].length-1) || i==j)
+					 if(i+j==(arr[i].length-1))
 						 System.out.print("* ");
+					 else if(i==j)
+						 System.out.print("@ ");
 					 else
 						 System.out.print(arr[i][j]+" ");
 		      }
