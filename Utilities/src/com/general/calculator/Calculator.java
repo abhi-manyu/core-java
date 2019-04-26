44e375d7f255426becd33f23bd81c699f186fa34
Abhi <abhim.moharana@gmail.com>
2019-04-26 10:44:35 +0530

    addition method created and configured

--- Utilities/src/com/general/calculator/Calculator.java
+++ Utilities/src/com/general/calculator/Calculator.java
@@ -2,11 +2,11 @@ package com.general.calculator;
 
 public class Calculator
 {
-   public static void sum(String num1, String num2)
+   public static int sum(String num1, String num2)
    {
-	   /*int num_1=Integer.parseInt(num1);
+	   int num_1=Integer.parseInt(num1);
 	   int num_2=Integer.parseInt(num1);
 	   int sum=num_1+num_2;
-	   return sum;*/
+	   return sum;
    }
 }
--- Utilities/src/com/general/calculator/Client.java
+++ Utilities/src/com/general/calculator/Client.java
@@ -1,9 +1,18 @@
 package com.general.calculator;
 
+import java.util.Scanner;
+
 public class Client
 {
 	public static void main(String[] args)
 	{
-       
+       Scanner scn=new Scanner(System.in);
+       System.out.println("enter first number");
+       String num1=scn.nextLine();
+       System.out.println("enter ur second number");
+       String num2=scn.nextLine();
+       int result=Calculator.sum(num1, num2);
+       System.out.println("ur sum is :"+result);
+       scn.close();
 	}
 }
