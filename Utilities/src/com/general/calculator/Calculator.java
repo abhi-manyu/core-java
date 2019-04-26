bbad7aa3f46b59ba164be87934d00ff6725ec210
Abhi <abhim.moharana@gmail.com>
2019-04-26 10:31:11 +0530

    created new calculator application.

---
+++ Utilities/src/com/general/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.general.calculator;
+
+public class Calculator
+{
+   public static void sum(String num1, String num2)
+   {
+	   /*int num_1=Integer.parseInt(num1);
+	   int num_2=Integer.parseInt(num1);
+	   int sum=num_1+num_2;
+	   return sum;*/
+   }
+}
---
+++ Utilities/src/com/general/calculator/Client.java
@@ -0,0 +1,9 @@
+package com.general.calculator;
+
+public class Client
+{
+	public static void main(String[] args)
+	{
+       
+	}
+}
